0de3a957c90e6e872eac8211db2077d2d9d3733f
nikita1211 <nikitakotak12@gmail.com>
2017-09-11 19:59:00 +0530

    Add files via upload

---
+++ check_img.py
@@ -0,0 +1,19 @@
+import urllib
+import os
+from mimetypes import guess_extension
+
+def check_img (str):
+	source = urllib.urlopen(url)
+	extension = guess_extension(source.info()['Content-Type'])
+	#print extension
+	if extension == ".png" or ".jpg" or ".svg":
+		print "an image"
+		string1 = "file03"
+		string2 = string1+extension
+		resource = urllib.urlopen(url)
+		output = open(string2,"wb")
+		output.write(resource.read())
+		output.close()
+
+url="https://dgplug.org/assets/img/header.png"
+check_img(url)
